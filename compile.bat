968d6f56a84065ac7c16f97d1150d311a28b569a
Egor <75483723+Spacice@users.noreply.github.com>
2026-06-14 19:58:01 +0400

    Added submodule Sodium

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
-g++ -static -O3 -o keygen -IE:./libsodium-master/src/libsodium/include -L. main.cpp -lsodium -lpthread
+g++ -static -O3 -o keygen -IE:./libsodium/src/libsodium/include -L. main.cpp -lsodium -lpthread
 pause
