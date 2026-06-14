282014289225a4938babe8537408cfd706a7f470
Egor <75483723+Spacice@users.noreply.github.com>
2026-06-14 20:23:48 +0400

    Changed compilation

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
-g++ -static -O3 -o keygen -IE:./libsodium/src/libsodium/include -L. main.cpp -lsodium -lpthread
+g++ -static -O3 -o keygen -IE:./libsodium-win64/include -L./libsodium-win64/lib main.cpp -lsodium -lpthread
 pause
